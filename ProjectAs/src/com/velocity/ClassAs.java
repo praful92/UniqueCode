28634860eea7a84623f3cd33d171be373f88aa96
praful <prafultell@gmail.com>
2024-02-02 17:20:59 +0530

    arraylist code

--- ProjectAs/src/com/velocity/ClassAs.java
+++ ProjectAs/src/com/velocity/ClassAs.java
@@ -1,5 +1,22 @@
 package com.velocity;
 
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class ClassAs {
 
+	public static void main(String[] args) {
+		
+		Set<Integer> set = new TreeSet<Integer>();
+		set.add(40);
+		set.add(30);
+		set.add(50);
+		set.add(10);
+		
+		System.out.println(set);
+		
+		ArrayList a = new ArrayList(set);
+		System.out.println(a);
+	}
 }
